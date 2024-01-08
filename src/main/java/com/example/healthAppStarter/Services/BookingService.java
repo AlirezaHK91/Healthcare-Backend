a7e25c9693bd89a9dbf7df42a9d90013f814f1fc
JohanBengtsson89 <johanbengtsson89@gmail.com>
2024-01-08 10:01:13 +0100

    Started with double booking function

--- src/main/java/com/example/healthAppStarter/Services/BookingService.java
+++ src/main/java/com/example/healthAppStarter/Services/BookingService.java
@@ -1,9 +1,16 @@
 package com.example.healthAppStarter.Services;
 
 import com.example.healthAppStarter.models.Booking;
+import com.example.healthAppStarter.models.Schedule;
 import com.example.healthAppStarter.repository.BookingRepository;
+import com.example.healthAppStarter.repository.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class BookingService {
@@ -11,9 +18,19 @@ public class BookingService {
     @Autowired
     BookingRepository bookingRepository;
 
+    @Autowired
+    ScheduleRepository scheduleRepository;
 
-    public Booking createBooking(Booking booking) {
-        return bookingRepository.save(booking);
+    public String createBooking(LocalDate date, Time time) {
+       List<Schedule> schedules = scheduleRepository.findByDateAndTime(date, time);
+       if (schedules != null && schedules.get(0).isAvailable()) {
+           Booking booking = new Booking();
+           booking.setSchedule(schedules.get(0));
+           bookingRepository.save(booking);
+           return "Booking confirmed from service";
+       } else {
+           return "Slot is not available from service";
+       }
     }
 
     public Booking getBookingById(Long id) {
--- src/main/java/com/example/healthAppStarter/controllers/BookingController.java
+++ src/main/java/com/example/healthAppStarter/controllers/BookingController.java
@@ -3,9 +3,16 @@ package com.example.healthAppStarter.controllers;
 import com.example.healthAppStarter.Services.BookingService;
 import com.example.healthAppStarter.models.Booking;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.Collections;
 
 @RestController
 @RequestMapping(value = "/api/auth")
@@ -16,8 +23,11 @@ public class BookingController {
 
     @PostMapping("/booking")
     @PreAuthorize("hasRole ('ROLE_USER')")
-    public Booking createBooking(@RequestBody Booking booking){
-        return bookingService.createBooking(booking);
+    public String createBooking(@RequestBody Booking booking){
+        LocalDate bookingDate = booking.getSchedule().getDate();
+        Time bookingTime = booking.getSchedule().getTime();
+        bookingService.createBooking(bookingDate, bookingTime);
+        return "Booking confirmed from controller";
     }
 
     @GetMapping("/booking/{id}")
